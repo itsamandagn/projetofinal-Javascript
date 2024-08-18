7628770dcd4b1dbc1d5b8ae2cdb42447c2edcdbe
Amanda <amandagn07@gmail.com>
2024-08-18 16:18:20 -0300

    cadastro de clientes

---
+++ cadastro.js
@@ -0,0 +1,23 @@
+const cliente = [];
+        const cadastrarCliente = () => {
+            cliente.nome = prompt("Digite seu nome");
+            cliente.endereco = prompt("Digite seu endereço");
+            cliente.email = prompt("Digite seu email");
+        }
+    
+    
+    cadastrarCliente();
+    console.log (cliente);
+        
+
+
+/*const form = document.getElementById(form);
+const nome = document.getElementById(inputname);
+const email = document.getElementById(inputEmail);
+const endereco = document.getElementById(inputAddress);
+const complementoEndereco = document.getElementById(inputComplement);
+const cidade = document.getElementById(inputCity);
+const estado = document.getElementById(inputState);
+const cep = document.getElementById(inputZip);*/
+
+
