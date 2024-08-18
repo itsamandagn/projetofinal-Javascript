b84e4abcd80ea53c26deff7ef5ffbe76a620c587
Amanda <amandagn07@gmail.com>
2024-08-18 17:19:23 -0300

    solicitando dados de clientes

--- cadastro.js
+++ cadastro.js
@@ -1,15 +1,15 @@
-const cliente = [];
-        const cadastrarCliente = () => {
-            cliente.nome = prompt("Digite seu nome");
-            cliente.endereco = prompt("Digite seu endereço");
-            cliente.email = prompt("Digite seu email");
-        }
-    
-    
-    cadastrarCliente();
-    console.log (cliente);
-        
+const cliente = {};
+const cadastrarCliente = () => {
+  cliente.nome = prompt("Digite seu nome");
+  cliente.endereco = prompt("Digite seu endereço");
+  cliente.email = prompt("Digite seu email");
 
+  console.log("Nome:" + cliente.nome);
+  console.log("Endereço:" + cliente.endereco);
+  console.log("E-mail:" + cliente.email);
+};
+
+cadastrarCliente();
 
 /*const form = document.getElementById(form);
 const nome = document.getElementById(inputname);
@@ -19,5 +19,3 @@ const complementoEndereco = document.getElementById(inputComplement);
 const cidade = document.getElementById(inputCity);
 const estado = document.getElementById(inputState);
 const cep = document.getElementById(inputZip);*/
-
-
