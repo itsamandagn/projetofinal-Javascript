16424aabd9fc72cedc7e169ca841b149c9b93aa2
Amanda <amandagn07@gmail.com>
2024-08-27 19:39:19 -0300

    atualizando cadastro

--- cadastro.js
+++ cadastro.js
@@ -1,31 +1,29 @@
-const cliente = {};
 const cadastrarCliente = () => {
-  cliente.nome = prompt("Digite seu nome");
-  cliente.endereco = prompt("Digite seu endereço:rua, número e bairro");
-  cliente.cidade = prompt("Digite sua cidade");
-  cliente.estado = prompt("Digite seu estado"); 
-  let emailValido = false;
-  while (!emailValido) {
-    cliente.email = prompt("Digite um email válido");
-    if (cliente.email.includes("@")) {
-      emailValido = true;
-    } else {
-      alert("Email inválido!");
-    }
+  const nome = document.getElementById("nome").value;
+  const email = document.getElementById("email").value;
+  const endereco = document.getElementById("endereco").value;
+  const complemento = document.getElementById("complemento").value;
+  const cidade = document.getElementById("cidade").value;
+  const estado = document.getElementById("estado").value;
+  const cep = document.getElementById("cep").value;
+  if (!email.includes("@")) {
+    alert("Email inválido!");
+    return;
   }
+  const cliente = {
+    nome: nome,
+    email: email,
+    endereco: endereco,
+    complemento: complemento,
+    cidade: cidade,
+    estado: estado,
+    cep: cep,
+  };
 
-  console.log("Nome:" + cliente.nome);
-  console.log(`Endereço: ${cliente.endereco}  ${cliente.cidade}  ${cliente.estado}`);
-  console.log("E-mail:" + cliente.email);
-};
-
-cadastrarCliente();
-
-/*const form = document.getElementById(form);
-const nome = document.getElementById(inputname);
-const email = document.getElementById(inputEmail);
-const endereco = document.getElementById(inputAddress);
-const complementoEndereco = document.getElementById(inputComplement);
-const cidade = document.getElementById(inputCity);
-const estado = document.getElementById(inputState);
-const cep = document.getElementById(inputZip);*/
+  console.log(cliente.nome);
+  console.log(
+    `${cliente.endereco}, ${cliente.complemento}, ${cliente.cidade} - ${cliente.estado}`
+  );
+  console.log(cliente.email);
+  console.log(cliente.cep);
+}
