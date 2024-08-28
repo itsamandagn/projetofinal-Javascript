9e18f2eabd501b222bf325a056c2aac4a86835b1
Amanda <amandagn07@gmail.com>
2024-08-28 13:38:32 -0300

    criar carrinho na pagina index

---
+++ script.js
@@ -0,0 +1,20 @@
+inicializarLoja = () => {
+    let containerProdutos = document.getElementById(`produtos`);
+
+
+
+
+
+}
+const itens = [
+    {
+        id: 0,
+        nome: "camisasmile",
+        tamanho: "tamanho",
+        quantidade: 0
+    }
+];
+
+
+
+inicializarLoja();
