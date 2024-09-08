3279411d62dad86fe863e49979b1939178501725
Amanda <amandagn07@gmail.com>
2024-09-08 13:40:24 -0300

    criando tabela de pedidos

--- pedido.js
+++ pedido.js
@@ -1,37 +1,38 @@
-document .getElementById("fechar-pedido") .addEventListener("click", function () {
-    const produto = document.getElementById("produtos").value;
-    const tamanho = document.getElementById("tamanhos").value;
-    const quantidade = parseInt(document.getElementById("quantidade").value);
+document.getElementById("fechar-pedido") .addEventListener("click", function () {
+      const produto = document.getElementById("produtos").value;
+      const tamanho = document.getElementById("tamanhos").value;
+      const quantidade = parseInt(document.getElementById("quantidade").value);
 
-    let valorUnitario;
-    switch (produto) {
-      case "Camisa Smile":
-        valorUnitario = 89.9;
-        break;
-      case "Camisa Listras":
-        valorUnitario = 79.9;
-        break;
-      case "Camisa Preta":
-        valorUnitario = 69.9;
-        break;
-      case "Camisa Azul":
-        valorUnitario = 69.9;
-        break;
-      default:
-        valorUnitario = 0;
-    }
+      let valorUnitario;
+      switch (produto) {
+        case "camisa Smile":
+          valorUnitario = 89.9;
+          break;
+        case "Camisa Listras":
+          valorUnitario = 79.9;
+          break;
+        case "Camisa Preta":
+          valorUnitario = 69.9;
+          break;
+        case "Camisa Azul":
+          valorUnitario = 69.9;
+          break;
+        default:
+          valorUnitario = 0;
+      }
 
-    const frete = 10.0;
-    const valorTotal = valorUnitario * quantidade + frete;
+      const frete = 10.0;
+      const valorTotal = valorUnitario * quantidade + frete;
 
-    const tabela = document.querySelector(".info-carrinho tbody");
-    const novaLinha = tabela.insertRow();
+      const tabela = document.querySelector(".info-carrinho tbody");
+      const novaTabela = tabela.insertRow();
 
-    novaLinha.innerHTML = `
+      novaTabela.innerHTML = `
             <td>${produto} - Tamanho: ${tamanho}</td>
             <td>${quantidade}</td>
             <td>R$ ${valorUnitario.toFixed(2)}</td>
             <td>R$ ${frete.toFixed(2)}</td>
             <td>R$ ${valorTotal.toFixed(2)}</td>
     `;
-  });
+    });
+  
