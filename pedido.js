7a1abd1b9dfcf3e48208d346f05ad14b3a205f45
Amanda <amandagn07@gmail.com>
2024-09-10 19:29:55 -0300

    adicionando item carrinho

--- pedido.js
+++ pedido.js
@@ -1,37 +1,15 @@
-document.getElementById("fechar-pedido").addEventListener("click", function () {
-  const produto = document.getElementById("produtos").value;
-  const tamanho = document.getElementById("tamanhos").value;
-  const quantidade = parseInt(document.getElementById("quantidade").value);
-
-  let valorUnitario;
-  switch (produto) {
-    case "Camisa Smile":
-      valorUnitario = 89.9;
-      break;
-    case "Camisa Listras":
-      valorUnitario = 79.9;
-      break;
-    case "Camisa Preta":
-      valorUnitario = 69.9;
-      break;
-    case "Camisa Azul":
-      valorUnitario = 69.9;
-      break;
-    default:
-      valorUnitario = 0;
-  }
-
-  const frete = 10.0;
-  const valorTotal = valorUnitario * quantidade + frete;
-
+document.addEventListener("DOMContentLoaded", function () {
+  const carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
   const tabela = document.querySelector(".info-carrinho tbody");
-    const novaLinha = tabela.insertRow();
 
+  carrinho.forEach(item => {
+    const novaLinha = tabela.insertRow();
     novaLinha.innerHTML = `
-            <td>${produto} - Tamanho: ${tamanho}</td>
-            <td>${quantidade}</td>
-            <td>R$ ${valorUnitario.toFixed(2)}</td>
-            <td>R$ ${frete.toFixed(2)}</td>
-            <td>R$ ${valorTotal.toFixed(2)}</td>
+      <td>${item.produto} - Tamanho: ${item.tamanho}</td>
+      <td>${item.quantidade}</td>
+      <td>R$ ${item.valorUnitario.toFixed(2)}</td>
+      <td>R$ ${item.frete.toFixed(2)}</td>
+      <td>R$ ${item.valorTotal.toFixed(2)}</td>
     `;
   });
+});
---
+++ script.js
@@ -0,0 +1,44 @@
+document.getElementById("fechar-pedido").addEventListener("click", function () {
+    const produto = document.getElementById("produtos").value;
+    const tamanho = document.getElementById("tamanhos").value;
+    const quantidade = parseInt(document.getElementById("quantidade").value);
+  
+    let valorUnitario;
+    switch (produto) {
+      case "Camisa Smile":
+        valorUnitario = 89.9;
+        break;
+      case "Camisa Listras":
+        valorUnitario = 79.9;
+        break;
+      case "Camisa Preta":
+        valorUnitario = 69.9;
+        break;
+      case "Camisa Azul":
+        valorUnitario = 69.9;
+        break;
+      default:
+        valorUnitario = 0;
+    }
+  
+    const frete = 10.0;
+    const valorTotal = valorUnitario * quantidade + frete;
+  
+    const carrinhoIten = {
+      produto,
+      tamanho,
+      quantidade,
+      valorUnitario,
+      frete,
+      valorTotal
+    };
+  
+    let carrinho = JSON.parse(localStorage.getItem('carrinho')) || [];
+    carrinho.push(carrinhoIten);
+    localStorage.setItem('carrinho', JSON.stringify(carrinho));
+  
+    alert("Produto adicionado ao carrinho!");
+  
+   
+    window.location.href = "pedido.html";
+  });
